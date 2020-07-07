5e16341acda848f895b4452e3ecefb32ed8cd99d
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-07-07 16:19:40 +0530

    commit

---
+++ comments/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+app.get('/posts/:id/comments',(req,res) =>{
+    
+})
+app.post('/posts/:id/comments',(req,res) =>{
+
+
+
+})
+
+app.listen(4001,() => {
+    console.log('Listning at 4001')
+})
